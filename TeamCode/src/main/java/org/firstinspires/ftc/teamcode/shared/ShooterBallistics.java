87cfe67778bcd28e03cf6417c4c2d26e1017ba6f
STIMDC <clubulstimdc@gmail.com>
2026-05-13 12:10:45 +0300

    Save

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/ShooterBallistics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shared/ShooterBallistics.java
@@ -9,9 +9,9 @@ public final class ShooterBallistics {
     // 2600 rpm around 2 m
     public static int rpmForDistanceMeters(double distanceMeters) {
         double g = 9.81;
-        double tan56 = 1.483;
-        double cos56sq = 0.312;
-        double k = 0.47; // efficiency
+        double tan56 = 0.869; //41 degrees
+        double cos56sq = 0.5696;
+        double k = 0.45; // efficiency
         double radius = 0.035; // shooter wheel radius in m
         double heightDiff = 0.7; // height diff in m
 
